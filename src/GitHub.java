cff0d86c128cbef280409a429f7b5464bdd26e18
shathamallak <52199429+shathamallak@users.noreply.github.com>
2019-07-15 22:32:07 +0300

    noe

--- src/GitHub.java
+++ src/GitHub.java
@@ -4,7 +4,7 @@ public class GitHub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("hello !! my name lara uu  ");
+		System.out.println("hello !! good luck  ");
 
 	}
 
